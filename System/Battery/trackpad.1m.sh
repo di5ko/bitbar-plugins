5f44829d787371b1337b8d61c8609c8a14353d95
Erik de Jong <erik@dotblue.nl>
2016-02-15 15:23:07 +0100

    Remove unneeded plugins

--- System/Battery/trackpad.1m.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-PERCENTAGE=`ioreg -n BNBTrackpadDevice | fgrep BatteryPercent |fgrep -v { | sed 's/[^[:digit:]]//g'`
-
-if [ "$PERCENTAGE" ]; then
-        echo "Trackpad: $PERCENTAGE%"
-fi
-
